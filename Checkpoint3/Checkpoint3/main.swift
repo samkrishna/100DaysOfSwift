3bf1b5918d767fddc8ad6e36db5e6ac77007e8e8
Sam Krishna <sam.krishna@gmail.com>
2022-03-02 12:47:37 -0800

    Add 'Checkpoint 3' as an explicit tool project for testing

---
+++ Checkpoint3/Checkpoint3/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  Checkpoint3
+//
+//  Created by Sam Krishna on 3/2/22.
+//
+
+import Foundation
+
+for i in 1...100 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
+        print("For \(i): FizzBuzz")
+        continue
+    }
+
+    if i.isMultiple(of: 3) {
+        print("For \(i): Fizz")
+    }
+    else if i.isMultiple(of: 5) {
+        print("For \(i): Buzz")
+    }
+    else {
+        print("For \(i): Nope")
+    }
+}
